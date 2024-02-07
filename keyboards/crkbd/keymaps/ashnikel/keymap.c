efa1d5b00c05884adbac91635055709938cdfdfc
Victor Medvedik <ashnikel@gmail.com>
2024-02-07 15:19:37 +0300

    upd

--- keyboards/crkbd/keymaps/ashnikel/keymap.c
+++ keyboards/crkbd/keymaps/ashnikel/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,
+      SC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SC_RSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_BSPC
                                       //`--------------------------'  `--------------------------'
@@ -69,6 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
+    // QK_BACKLIGHT_TOGGLE 	BL_TOGG 	Turn the backlight on or off
+    // QK_BACKLIGHT_STEP 	BL_STEP 	Cycle through backlight levels
+    // QK_BACKLIGHT_ON 	BL_ON 	Set the backlight to max brightness
+    // QK_BACKLIGHT_OFF 	BL_OFF 	Turn the backlight off
+    // QK_BACKLIGHT_UP 	BL_UP 	Increase the backlight level
+    // QK_BACKLIGHT_DOWN 	BL_DOWN 	Decrease the backlight level
+    // QK_BACKLIGHT_TOGGLE_BREATHING 	BL_BRTG 	Toggle backlight breathing
   )
 };
 
